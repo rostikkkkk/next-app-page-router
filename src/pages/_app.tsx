89532ed0b1caf6f7a5b3c2bd1fc5a2e9511f6b62
rostikkkkk <kievstarpro01082017@gmail.com>
2024-06-24 20:04:29 +0300

    Fix: remove UI flickering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "../../i18n";
-import AntdStyledComponentsRegistry from "@/AntdStyledComponentsRegistry";
 import { QueryParamProvider } from "use-query-params";
 import NextAdapterApp from "next-query-params/app";
 import { StoreProvider } from "@/store/provider";
+import { ConfigProvider } from "antd";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StoreProvider>
       <QueryParamProvider adapter={NextAdapterApp}>
-        <AntdStyledComponentsRegistry>
+        <ConfigProvider>
           <Component {...pageProps} />
-        </AntdStyledComponentsRegistry>
+        </ConfigProvider>
       </QueryParamProvider>
     </StoreProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-export default function Document() {
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import type { DocumentContext } from "next/document";
+export default function MyDocument() {
   return (
     <Html lang="en">
       <Head />
@@ -17,3 +18,29 @@ export default function Document() {
     </Html>
   );
 }
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) =>
+        (
+          <StyleProvider cache={cache}>
+            <App {...props} />
+          </StyleProvider>
+        ),
+    });
+
+  // @ts-ignore
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
